1bed3f7ea3a03377f858fadcd7f1a849453e4a3c
victor.lopez <victor.lopez@greentwip.com>
2016-12-15 11:48:50 -0700

    Add hold animations

--- Assets/Scripts/Item/ItemBlock.cs
+++ Assets/Scripts/Item/ItemBlock.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemBlock : MonoBehaviour {
-    float ray_length = 10;
+    float ray_length = 16;
     float ray_offset = 8;
 
     bool is_active = true;
+
+    Animator animator;
 
     public enum ItemKind
     {
@@ -22,7 +24,7 @@ public class ItemBlock : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
+        animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -33,29 +35,14 @@ public class ItemBlock : MonoBehaviour {
         var ray = transform.FindChild("ray");
 
         var ray_start = new Vector2(ray.transform.position.x, ray.transform.position.y + ray_offset);
-        var ray_right = new Vector2(ray.transform.position.x + ray_length, ray.transform.position.y + ray_offset);
         var ray_left = new Vector2(ray.transform.position.x - ray_length, ray.transform.position.y + ray_offset);
+        var ray_right = new Vector2(ray.transform.position.x + ray_length, ray.transform.position.y + ray_offset);
 
-        RaycastHit2D[] right_hits = Physics2D.LinecastAll(ray_start, ray_right);
         RaycastHit2D[] left_hits = Physics2D.LinecastAll(ray_start, ray_left);
+        RaycastHit2D[] right_hits = Physics2D.LinecastAll(ray_start, ray_right);
 
-        Debug.DrawLine(ray_start, ray_right, Color.red);
         Debug.DrawLine(ray_start, ray_left, Color.red);
-
-        foreach (RaycastHit2D hit in right_hits)
-        {
-            var collider = hit.collider;
-            if (collider != null)
-            {
-                if (collider.gameObject.tag == "Shell")
-                {
-                    if (is_active)
-                    {
-                        is_active = false;
-                    }
-                }
-            }
-        }
+        Debug.DrawLine(ray_start, ray_right, Color.red);
 
         foreach (RaycastHit2D hit in left_hits)
         {
@@ -64,13 +51,33 @@ public class ItemBlock : MonoBehaviour {
             {
                 if (collider.gameObject.tag == "Shell")
                 {
-                    if (is_active)
+                    var shell = collider.gameObject.GetComponent<Shell>();
+                    if (shell.is_moving())
                     {
                         is_active = false;
                     }
                 }
             }
         }
+
+        foreach (RaycastHit2D hit in right_hits)
+        {
+            var collider = hit.collider;
+            if (collider != null)
+            {
+                if (collider.gameObject.tag == "Shell")
+                {
+                    var shell = collider.gameObject.GetComponent<Shell>();
+                    if (shell.is_moving())
+                    {
+                        is_active = false;
+                    }
+                    
+                }
+            }
+        }
+
+        animator.SetBool("is_active", is_active);
 
     }
 
---
+++ Assets/Scripts/Player/Trigger.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void Update()
+    {
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision enter 2d");
+    }
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        var controller = transform.parent.GetComponent<Controller>();
+
+        // If the player enters the trigger zone...
+        if (other.gameObject.tag == "Shell")
+        {
+            var shell = other.gameObject.GetComponent<Shell>();
+
+            if (shell.is_moving())
+            {
+                // ray tracing
+                var ray = controller.transform.FindChild("ray");
+
+                var ray_start = ray.transform.position;
+                var ray_top = new Vector2(ray.transform.position.x, ray.transform.position.y - 8);
+
+                RaycastHit2D[] top_hits = Physics2D.LinecastAll(ray_start, ray_top);
+
+                foreach (RaycastHit2D hit in top_hits)
+                {
+                    var collider = hit.collider;
+                    if (collider != null)
+                    {
+                        if (collider.gameObject.tag == "Shell")
+                        {
+                            var player_rigid_body = controller.GetComponent<Rigidbody2D>();
+                            player_rigid_body.velocity = new Vector2(player_rigid_body.velocity.x, 0);
+
+                            if (Input.GetKey(KeyCode.Space))
+                            {
+                                player_rigid_body.AddForce(new Vector2(0, controller.SmallJumpForce * 2f));
+                            }
+                            else
+                            {
+                                player_rigid_body.AddForce(new Vector2(0, controller.SmallJumpForce * 1.25f));
+                            }
+                            
+                            SoundManager.instance.PlaySingle(controller.StompSoundEffect);
+
+                            shell.is_moving(false);
+                            var rigid_body = shell.GetComponent<Rigidbody2D>();
+                            rigid_body.velocity = new Vector2();
+
+                        }
+                    }
+                }
+
+            }
+            else
+            {
+                // verify pressed key
+                if (Input.GetKey(KeyCode.Z))
+                {
+                    controller.pick_item_up(other.gameObject);
+                }
+                else
+                {
+                    controller.kick_item(other.gameObject);
+                }
+            }
+        }
+
+    }
+}
